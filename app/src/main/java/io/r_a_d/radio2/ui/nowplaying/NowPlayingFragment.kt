56057d2781f018678535c31664da11c6448ffc8a
yattoz <yatto_963@hotmail.fr>
2019-10-23 14:44:44 +0200

    small rework of interface and colors.

--- app/src/main/java/io/r_a_d/radio2/ui/nowplaying/NowPlayingFragment.kt
+++ app/src/main/java/io/r_a_d/radio2/ui/nowplaying/NowPlayingFragment.kt
@@ -8,10 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
-import android.widget.ProgressBar
-import android.widget.SeekBar
-import android.widget.TextView
+import android.widget.*
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.SavedStateViewModelFactory
