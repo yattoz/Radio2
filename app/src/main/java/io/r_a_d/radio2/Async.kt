58d11ed3b7810baa966d29a0d575e01e7344afa9
yattoz <yatto_963@hotmail.fr>
2019-11-04 18:19:23 +0100

    added try...catch to avoid crash when no network connection
    Signed-off-by: yattoz <yatto_963@hotmail.fr>

--- app/src/main/java/io/r_a_d/radio2/Async.kt
+++ app/src/main/java/io/r_a_d/radio2/Async.kt
@@ -1,17 +1,32 @@
 package io.r_a_d.radio2
 
 import android.os.AsyncTask
+import android.util.Log
 
 class Async(val handler: () -> Any?, val post: (Any?) -> Unit = {}) : AsyncTask<Void, Void, Any>() {
     init {
-        execute()
+        try {
+            execute()
+        } catch (e: Exception)
+        {
+            Log.d(tag,e.toString())
+        }
     }
 
     override fun doInBackground(vararg params: Void?): Any? {
-        return handler()
+        try {
+            return handler()
+        } catch (e: Exception) {
+            Log.d(tag,e.toString())
+        }
+        return null
     }
 
     override fun onPostExecute(result: Any?) {
-        post(result)
+        try {
+            post(result)
+        } catch (e: Exception) {
+            Log.d(tag,e.toString())
+        }
     }
 }
